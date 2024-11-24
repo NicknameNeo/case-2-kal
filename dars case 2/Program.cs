e3e8e1df483018fd639a7b4ca287865e53dbcb0f
NicknameNeo <abdulloevaslan96@gmail.com>
2024-11-24 13:48:15 +0300

    My project

---
+++ dars case 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace dars_case_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = a + b;
+                    Console.WriteLine(result);
+                    break;
+                case "-" :
+                    result = a - b;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = a * b;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    if (a >= 0 && b != 0)
+                    {
+                        result = a / b;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide {0} by zero", operation);
+                    }
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+        }
+    }
+}
